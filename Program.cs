0fd2ba3a5c02e382a00e19c4c8b37e907874fecc
Ruan Almeida <oruanvinicios@gmail.com>
2025-08-28 22:12:30 -0300

    primeira estrutura

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+double baseTriangulo;
+Console.WriteLine("Digite a base do triângulo:");
+// Continua pedindo a base até que um número válido seja inserido
+while (!double.TryParse(Console.ReadLine(), out baseTriangulo) || baseTriangulo <= 0)
+{
+    Console.WriteLine("Entrada inválida. Por favor, digite um número positivo para a base:");
+}
+
+double alturaTriangulo;
+Console.WriteLine("Digite a altura do triângulo:");
+// Continua pedindo a altura até que um número válido seja inserido
+while (!double.TryParse(Console.ReadLine(), out alturaTriangulo) || alturaTriangulo <= 0)
+{
+    Console.WriteLine("Entrada inválida. Por favor, digite um número positivo para a altura:");
+}
+
+double areaDoTriangulo = (baseTriangulo * alturaTriangulo) / 2.0;
+
+// Formata a saída para exibir o número com 2 casas decimais usando ":F2"
+Console.WriteLine($"A área do triângulo equivale a: {areaDoTriangulo:F2}");
